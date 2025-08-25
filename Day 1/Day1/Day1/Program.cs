dac472b3964993d2d38c1c72e0dba727a21a7bc9
das5202 <devikarani1182001@gmail.com>
2025-08-25 08:27:22 +0530

    Basic

---
+++ Day 1/Day1/Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Day1;
+
+namespace nee
+{
+    class Devika
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Devika");
+            Console.ReadLine();
+            second a=new second();
+            a.userDetails();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day 1/Day1/Day1/second.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+     class second
+    {
+        public void userDetails()
+        {
+            Console.WriteLine("Enter your name and Age");
+            string Name = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Hello {Name} your age is {age}");
+
+
+        }
+
+    }
+}
