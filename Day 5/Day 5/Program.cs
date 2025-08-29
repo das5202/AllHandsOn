5f36ae82c6775db71e26acd14f44e8167c66e1aa
das5202 <devikarani1182001@gmail.com>
2025-08-29 23:26:58 +0530

    Methods & Function Overloading,Classes and Objects

---
+++ Day 5/Day 5/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace methods
+{
+    class Day5
+    {
+        public static void Main(String[] args)
+        {
+            //Implement Add() with int, float & double parameters
+            Console.WriteLine("Implementing methods");
+            Add d = new Add();
+            d.add(2, 3);
+            d.add(2.5f, 3.4);
+            d.add(9.999, 8.999);
+            calculator cal = new calculator();
+            cal.calc(2,3);
+            cal.calc(2.3f, 3.4f);
+            cal.calc(2, 3,"*");
+            statexam.stame("Devika", 23);
+            
+        }
+        class Add
+        {
+            public void add(int x, int y)
+            {
+                Console.WriteLine("int method : "+ (x + y) );
+            }
+            public void add(float x, float y)
+            {
+                Console.WriteLine("Float method : " + (x + y));
+            }
+            public void add(double x, double y)
+            {
+                Console.WriteLine("double  method : " + (x + y));
+            }
+        }
+        class calculator
+        {
+            public void calc(int x, int y)
+            {
+                Console.WriteLine(" calculator int : " + (x + y));
+            }
+            public void calc(float x, float y)
+            {
+                Console.WriteLine(" calculator Float :" + (x + y));
+            }
+            public void calc(int x, int y, string op)
+            {
+                switch (op)
+                {
+                    case "+":
+                        Console.WriteLine("if + then : " + (x + y));
+                        break;
+                    case "-":
+                        Console.WriteLine("if - then :  " + (x - y));
+                        break;
+                    case "*":
+                        Console.WriteLine("if * then :" + (x * y));
+                        break;
+                    case "/":
+                        if (y != 0)
+                            Console.WriteLine("if / then : " + (x / y));
+                        else
+                            Console.WriteLine("cannot be divided by zero");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Operator");
+                        break;
+
+
+                }
+            }
+        }
+             class statexam
+            {
+                public static void stame(string name,int age)
+                {
+                    Console.WriteLine($"Hello my name is {name} and age is {age}");
+                }
+            }
+           
+
+        
+    }
+}
---
+++ Day 6/Day6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace day6
+{
+    class person
+    {
+
+        //Create a Person class with name, age, and a method Introduce()
+        public string name;
+        public int age;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello i'm {name} and my age is :{age}");
+        }
+    }
+    class student
+    {
+        //Implement a Student class with static count of students
+        public string Section;
+        public int Rollnum;
+        public static int Count;
+        public student(string section,int rollnum)
+
+        {
+            Section = section;
+            Rollnum = rollnum;
+            Count++;
+        }
+        public void Studetails()
+        {
+            Console.WriteLine($"students in {Section} and Roll numbers are :{Rollnum}");
+        }
+
+
+    }
+
+     class program 
+    {
+        public static void Main(string[] args)
+        {
+            person meth = new person();
+            meth.name = "Devika";
+            meth.age = 23;
+            meth.Introduce();
+            student s1 = new student("A", 001);
+            s1.Studetails();
+            student s2 = new student("B", 002);
+            s2.Studetails();
+            student s3 = new student("C", 003);
+            s3.Studetails();
+            student s4 = new student("D", 004);
+            s4.Studetails();
+            Console.WriteLine($"Number of students are {student.Count}");
+        }
+    }
+
+}
