9ea8eb9bdbc1796af4dd783cad9bc70b6f127f64
das5202 <devikarani1182001@gmail.com>
2025-08-31 22:15:02 +0530

    Encapsulation & Properties ,Constructors & Destructors

---
+++ Day 7/Day7/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace enacap
+{
+    class bankAccount
+    {
+        //Implement a BankAccount class with private balance and deposit/withdraw methods
+
+
+        private int balance;
+        //Random blance =new Random(10000,20000)
+
+        public bankAccount()
+        {
+            Random random = new Random();
+            balance = random.Next(10000, 20001);
+        }
+        public void deposit(int money)
+        {
+
+            balance += money;
+        }
+        public void withdraw(int money)
+        {
+
+
+            if (balance >= money)
+            {
+                balance -= money;
+            }
+            else
+            {
+                Console.WriteLine("Insuffiencient Balance");
+            }
+        }
+        public int getBalance()
+        {
+            return balance;
+        }
+
+    }
+    class EmployeeDetails
+    {
+        //Use properties to manage employee details securely
+        private string name;
+        private int id;
+        private int salary;
+        public string Employename
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Employid
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int Employesalary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+    }
+
+
+   
+        class program
+        {
+            public static void Main(string[] args)
+            {
+                bankAccount b = new bankAccount();
+                Console.WriteLine($"Your current Balance:{b.getBalance()}");
+                Console.WriteLine("Enter Deposit amount");
+                int dep = Convert.ToInt32(Console.ReadLine());
+                b.deposit(dep);
+                Console.WriteLine($"Your current Balance:{b.getBalance()}");
+                Console.WriteLine("Enter withdraw amount");
+                int with = Convert.ToInt32(Console.ReadLine());
+                b.withdraw(with);
+                Console.WriteLine($"Your current Balance:{b.getBalance()}");
+            EmployeeDetails emp = new EmployeeDetails();
+            Console.WriteLine("Enter Employee Name:");
+            emp.Employename = Console.ReadLine();
+            Console.WriteLine("Enter Employee Id:");
+            emp.Employid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Salary:");
+            emp.Employesalary =Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Employe Name:{emp.Employename},Employe Id :{emp.Employid},Employe Salary:{emp.Employesalary}");
+
+
+
+
+
+
+        }
+        }
+
+    
+}
---
+++ Day 8/Day 8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+
+namespace cosntructor
+{
+    //Implement a Car class with constructor overloading
+    class car
+    {
+        public car()
+        {
+            Console.WriteLine("Hello i'm car ");
+        }
+        public car(string name)
+        {
+            
+            Console.WriteLine(name);
+        }
+        public car(string name,int price)
+        {
+            
+            Console.WriteLine($"{name},{price}");
+        }
+    }
+
+//Write a Book class where objects initialize with constructor values
+    class Book
+    {
+        public string BookName;
+        public string AuthorName;
+        public int Price;
+        public Book(string bookName, string authorName, int price)
+        {
+            BookName = bookName;
+            AuthorName = authorName;
+            Price = price;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Book Name: {BookName}, Author: {AuthorName}, Price: {Price}");
+
+        }
+    }
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            car c = new car();
+            car c1= new car("Range Rover");
+            car c2 = new car("Audi", 3500000);
+            Book b = new Book("Atomic Habits", "san", 340);
+            b.Display();
+        }
+    }
+}
