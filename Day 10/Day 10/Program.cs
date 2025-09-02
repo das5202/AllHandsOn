f24177ab7b4f2145198c24eb1f48b397ef8749aa
das5202 <devikarani1182001@gmail.com>
2025-09-02 22:06:09 +0530

    Inheritance & Method Overriding,Abstraction & Interfaces

---
+++ Day 10/Day 10/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace day10
+{
+    abstract class Animal
+    {
+        //Create an abstract class Animal and implement Dog and Cat classes
+        public abstract void sound();
+        
+    }
+    class cat:Animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Cat is Making sound Meow");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void sound()
+        {
+            Console.WriteLine("Dog is making sound Bow");
+        }
+    }
+
+    interface IShape
+    {
+        public void Area();
+        public void Perimeter();
+    }
+    class Circle : IShape
+    {
+        public int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+        public void Area()
+        {
+            double x = Math.PI * radius * radius;
+            Console.WriteLine($"Area of circle :{x}");
+        }
+        public void Perimeter()
+        {
+           double  y= 2*Math.PI * radius;
+            Console.WriteLine($"Perimeter of circle :{y}");
+        }
+    }
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            cat c= new cat();   
+            c.sound();
+            Dog d = new Dog();
+            d.sound();
+            //Animal a1 = new Cat();
+            //Animal a2 = new Dog();
+            //a1.sound();
+            //a2.sound();
+            IShape s = new Circle(3); //or Circle s= new Circle(3);
+            s.Area();
+            s.Perimeter();
+
+
+        }
+    }
+}
---
+++ Day 9/Day 9/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace day9
+{
+    class vehicle
+    {
+        //Create a Vehicle class and a Car class inheriting from it
+
+
+        public void Brand()
+        {
+            Console.WriteLine("In vehicles so many brands are there");
+
+        }
+        public void speed()
+        {
+            Console.WriteLine("Speed of the brand car depends on the engine");
+        }
+        public virtual void startEngine()
+        {
+            Console.WriteLine("Engine will start when turn on key");
+        }
+
+    }
+    class car : vehicle
+    {
+        // Override a StartEngine() method in the Car class
+        public void carbrand()
+        {
+            Console.WriteLine("The brand of our car is Audi");
+        }
+        public void carspeed(int speed)
+        {
+            Console.WriteLine($"Speed of Audi car is {speed}");
+        }
+        public override void startEngine()
+        {
+            Console.WriteLine("Audi engine will start after gviing key");
+        }
+    }
+
+    class progam
+        {
+        public static void Main(string[] args)
+        {
+            car c = new car();
+            c.Brand();
+            c.carbrand();
+           c.speed();
+           c.carspeed(200);
+           c.startEngine();
+          
+            
+        }
+        }
+}
